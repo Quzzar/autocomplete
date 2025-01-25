2899cbeb814cb7fdd07feafdb7a5c8af51e785e8
Quzzar <aaronncassar@gmail.com>
2025-01-25 01:57:49 -0500

    Already dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import MainInput from './MainInput';
 
 export default function App() {
   return (
-    <MantineProvider theme={theme}>
+    <MantineProvider theme={theme} forceColorScheme='dark'>
       <MainInput />
     </MantineProvider>
   );
